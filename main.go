0bfdab0be12288f6ead87d7f765f64ebc95f7130
raisyadjullfikar <raisyadjullfikar@upi.edu>
2026-03-05 22:41:41 +0700

    update: learn if syntax and for in go lang

--- main.go
+++ main.go
@@ -7,4 +7,20 @@ import (
 
 func main() {
 	fmt.Println("Hello World", uuid.NewString())
+
+	a := 1
+	if a > 0 {
+		fmt.Println("A is greater than 0")
+	} else {
+		fmt.Println("A is less than 0")
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("i is", i)
+	}
+
+	i := []int{1, 2, 3, 4, 5}
+	for index, value := range i {
+		fmt.Println("The index and value are: ", index, value)
+	}
 }
